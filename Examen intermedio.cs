a74ef534dae553f7b3d8efa2d66694d8af46e955
Yerlin-D <122233359+Yerlin-D@users.noreply.github.com>
2023-02-13 20:30:06 -0400

    Add files via upload

---
+++ Examen intermedio.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    //Creación de función ROT13
+    static class rot13
+    {
+        public static string descifrar(string newfrase)
+        {
+            char[] arreglo = newfrase.ToCharArray();
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                int x = (int)arreglo[i];
+                if (x >= 'A' && x <= 'Z')
+                {
+                    if (x > 'M')
+                    {
+                        x -= 13;
+                    }
+                    else
+                    {
+                        x += 13;
+                    }
+                }
+                arreglo[i] = (char)x;
+            }
+            return new string(arreglo);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string frase;
+        Console.WriteLine("Digite la palabra o frase que desea descifrar: ");
+        frase = Console.ReadLine();
+
+        //Llamamos a la función ROT13, creada al inicio del codigo
+        frase = rot13.descifrar(frase);
+        Console.WriteLine("El texto descifrado: ");
+        Console.WriteLine(frase);
+
+    }
+
+}
